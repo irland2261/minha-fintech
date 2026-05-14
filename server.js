6f136e821810bc8295459a3eea25129f19791417
irland2261 <irlandezesvoadores@gmail.com>
2026-05-14 16:29:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const app = express();
 
 app.use(express.json());
 
+// carteira simples
+let saldo = 1000;
+
+// pagar
+app.post("/pay", (req, res) => {
+  const { valor } = req.body;
+
+  if (valor > saldo) {
+    return res.status(400).json({ error: "Saldo insuficiente" });
+  }
+
+  saldo -= valor;
+
+  res.json({
+    message: "Pagamento aprovado 💳",
+    saldoAtual: saldo
+  });
+});
+
+// ver saldo
+app.get("/balance", (req, res) => {
+  res.json({ saldo });
+});
+
 app.get("/", (req, res) => {
-  res.json({ status: "API rodando 🚀" });
+  res.send("API fintech rodando 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("rodando");
-});
+app.listen(PORT);
