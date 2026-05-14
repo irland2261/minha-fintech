df62d18036d95f345f4f3d261a15030a5a8eb198
irland2261 <irlandezesvoadores@gmail.com>
2026-05-14 20:53:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ ok: true, message: "API WhatsApp online 🚀" });
+  res.json({
+    ok: true,
+    message: "API WhatsApp online 🚀"
+  });
 });
 
 app.post("/send", async (req, res) => {
@@ -13,24 +19,31 @@ app.post("/send", async (req, res) => {
 
   try {
     const response = await axios.post(
-      `https://graph.facebook.com/v19.0/${process.env.1307373424706833}/messages`,
+      `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
       {
         messaging_product: "whatsapp",
         to: number,
         type: "text",
-        text: { body: message }
+        text: {
+          body: message
+        }
       },
       {
         headers: {
-          Authorization: `Bearer ${process.env.EAAOH3cLVZCwYBRbsG9MPPfQOAE2zRkgaZB5qKEIGz5tonR3LH3ZASZBRBK4sCtK2cIPhkQeAurnE3pZBjnlfZAw2pVHhfy3LdhUTbclQBPnXekn9fZBbr5j5yzAsfZAZCKjczcsKOslPBMXZA5CzzSCuHzdsZB4uUrtTXOmZAACVHZBYJcvPhaIpgyfq99aHfvibUZANyHtzA86OAClZCE6kFcZBLbwIqFLOft4pDQLWrSMxS53EhroZAsYCYg9N2V0Ul7n5I9kiVo5J4dVnIhZA4LqTqETNAC}`,
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
           "Content-Type": "application/json"
         }
       }
     );
 
     res.json(response.data);
+
   } catch (err) {
-    res.status(500).json(err.response?.data || err.message);
+    res.status(500).json(
+      err.response?.data || {
+        error: err.message
+      }
+    );
   }
 });
 
