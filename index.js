dbcd9c97223424ab3d932d0d3bd21fa880a26f82
irland2261 <irlandezesvoadores@gmail.com>
2026-05-14 21:55:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-app.get("/painel", (req, res) => {
-  res.send(`
-    <html>
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json(    <html>
       <body style="font-family: Arial; padding: 30px;">
         <h2>Enviar WhatsApp 🚀</h2>
 
@@ -33,3 +41,38 @@ app.get("/painel", (req, res) => {
     </html>
   `);
 });
+
+app.post("/send", async (req, res) => {
+  const { number, message } = req.body;
+
+  try {
+    const response = await axios.post(
+      `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: number,
+        type: "text",
+        text: {
+          body: message
+        }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (err) {
+    res.status(500).json(
+      err.response?.data || {
+        error: err.message
+      }
+    );
+  }
+});
+
+module.exports = app;
